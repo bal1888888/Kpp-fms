3c558fcdd83ee70577fc2a02033cedf30669b5d9
kpp-mining <iqbalinsani1017@gmail.com>
2026-09-15 11:40:03 +0700

    fix: muat jalur HM langsung CCR

--- wib-time.js
+++ wib-time.js
@@ -123,6 +123,7 @@
 
   if(root.KPP_ACTIVE_PAGE==="ccr"){
     loadPageScript("ccr-auto-quota-v1.js?v=20260913b2");
+    onDomReady(()=>loadPageScript("ccr-direct-hm-v1.js?v=20260915a1",{async:false}));
   }
 
   if(root.KPP_ACTIVE_PAGE==="logsheet-editor"){
